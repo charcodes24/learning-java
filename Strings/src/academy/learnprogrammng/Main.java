61c8797fe6187b9b94a8feec26952e99cb4e8e47
charcodes24 <charcodes24@gmail.com>
2021-12-20 14:07:49 -0800

    Learned about String, string immutability and what happens when you combine string with integer and string with double

--- Strings/src/academy/learnprogrammng/Main.java
+++ Strings/src/academy/learnprogrammng/Main.java
@@ -3,6 +3,31 @@ package academy.learnprogrammng;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //byte *THE LEAST COMMON
+        //short *THE LEAST COMMON
+        //int *MOST COMMON
+        //long
+        //float
+        //double *MOST COMMON
+        //char
+        //boolean *MOST COMMON
+
+        String myString = "This is a string";
+        System.out.println("My String is = " + myString);
+        myString = myString + ", and this is more";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + " \u00A9 2019";
+        System.out.println("myString is equal to " + myString);
+        String numberString = "250.55";
+        numberString = numberString + "49.95"; //concatenates the strings
+        System.out.println("numberString = " + numberString);
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt; //trying to add int to String > converts value to string and appends
+        System.out.println("lastString = " + lastString);
+        double doubleNumber = 120.47d;
+        lastString = lastString + doubleNumber;
+        System.out.println(lastString);
     }
 }
