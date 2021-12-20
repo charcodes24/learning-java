476c7625323918c5ec69cf3dc4301837908c2fce
charcodes24 <charcodes24@gmail.com>
2021-12-20 11:24:40 -0800

    Learned about unicode characters and booleans

---
+++ CharAndBoolean/src/academy/learnprogrammng/Main.java
@@ -0,0 +1,20 @@
+package academy.learnprogrammng;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println("CHAR " + myChar);
+        System.out.println("UNICODE " + myUnicodeChar);
+        char myCopyRight = '\u00A9';
+        System.out.println("COPYRIGHT " + myCopyRight);
+
+        boolean myTrue = true;
+        boolean myFalse = false;
+        System.out.println("TRUE " + myTrue);
+        System.out.println("False " + myFalse);
+
+        boolean isCustomerOverTwentyOne = true; //common variable name to be question
+}
