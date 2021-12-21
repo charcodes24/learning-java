68aade3b871806b2fb6bf52808e3ceb99ae62821
charcodes24 <charcodes24@gmail.com>
2021-12-20 18:03:10 -0800

    Ternary operators

--- Operators/src/academy/learnprogrammng/Main.java
+++ Operators/src/academy/learnprogrammng/Main.java
@@ -75,5 +75,12 @@ public class Main {
         if (isCar) {
             System.out.println("This is not supposed to happen");
         }
+
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("wasCar is true");
+        }
     }
 }
