ceefd287fbdc2225419e288ecbe2e4075fee4498
charcodes24 <charcodes24@gmail.com>
2021-12-27 15:17:30 -0800

    Practice with for loops in Java

--- SwitchStatement/src/academy/learnprogrammng/Main.java
+++ SwitchStatement/src/academy/learnprogrammng/Main.java
@@ -58,6 +58,33 @@ public class Main {
         }
 
         printDayOfTheWeek(-6);
+
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+        System.out.println("10,000 at 3% interest = " + calculateInterest(10000.0, 3.0));
+        System.out.println("10,000 at 4% interest = " + calculateInterest(10000.0, 4.0));
+        System.out.println("10,000 at 5% interest = " + calculateInterest(10000.0, 5.0));
+
+        for(int i = 8; i >= 2; i--) {
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
+        }
+
+        System.out.println("*********************");
+
+        isPrime(7);
+    }
+
+    public static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= n/2; i++) {
+            if ((n & i) == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 
@@ -88,5 +115,11 @@ public class Main {
                 System.out.println("Invalid day");
                 break;
         }
+
+    }
+
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate/100));
     }
 }
