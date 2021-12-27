9f10d34207e8d48480f2857e9a44cc69fd8077b4
charcodes24 <charcodes24@gmail.com>
2021-12-27 08:24:50 -0800

    Benefits of Whitespace and Indentation for code readability

--- StatementsWhiteSpaceAndIndenting/src/academy/learnprogrammng/Main.java
+++ StatementsWhiteSpaceAndIndenting/src/academy/learnprogrammng/Main.java
@@ -3,6 +3,19 @@ package academy.learnprogrammng;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        //statement is entire line (datatype with expression and semicolon)
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        //Java can interpret whitespace appropriately
+        System.out.println("This is" +
+                "another" +
+                "still more.");
+
+        //use whitespace to make your code more readable!!
+        //Code > Reformat code or command option L
     }
 }
