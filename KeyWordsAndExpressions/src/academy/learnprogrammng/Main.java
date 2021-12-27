2a7639ad299e26215a28a9a37c6d5ea9fae3433f
charcodes24 <charcodes24@gmail.com>
2021-12-27 08:15:52 -0800

    Explanation of what Java expressions are

--- KeyWordsAndExpressions/src/academy/learnprogrammng/Main.java
+++ KeyWordsAndExpressions/src/academy/learnprogrammng/Main.java
@@ -4,5 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //a mile is equal to 1.609344 kilometres
+        //expression is variables, values and operators
+        //expression comes after declaration of data type
+        double kilometres = (100 * 1.609344);
+        int highScore = 50;
+
+        if (highScore == 50) {
+            //this is calling method with expression in double quotes
+            System.out.println("This is an expression");
+        }
+
+
+        //CHALLENGE:
+        //score = 100
+        //score > 99
+        //"You got the high score"
+        //score = 0
     }
 }
