42fd09f8f33054e4286fc63f77cf832445604b13
charcodes24 <charcodes24@gmail.com>
2022-01-03 11:33:41 -0800

    Explanation of class constructors with examples.

---
+++ Constructors/src/academy/learnprogrammng/Account.java
@@ -0,0 +1,83 @@
+package academy.learnprogrammng;
+
+public class Account {
+
+    private String number;
+    private double balance;
+    private String customerName;
+    private String customerEmailAddress;
+    private String customerPhoneNumber;
+
+    public Account() {
+        //this is calling another constructor within a constructor
+        //if you try and create an object without parameters, i'm going to use these default parameters
+        //if you wanna create object with all fields set with a value
+        //this needs to be the first line executed
+        this("56789", 2.50, "Default name", "Default address", "Default phone");
+        System.out.println("Empty constructor called");
+    }
+
+    public Account(String number, double balance, String customerName, String customerEmailAddress, String customerPhoneNumber) {
+        System.out.println("Account constructor with parameters called.");
+        this.number = number;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.customerEmailAddress = customerEmailAddress;
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
+
+    public void deposit(double depositAmount) {
+        this.balance += depositAmount;
+        System.out.println("Deposit of " + depositAmount + " made. New balance is " + this.balance);
+    }
+
+    public void withdrawal(double withdrawalAmount) {
+        if (this.balance - withdrawalAmount < 0) {
+            System.out.println("Only " + this.balance + " available. Withdrawal not processed.");
+        } else {
+            this.balance -= withdrawalAmount;
+            System.out.println("Withdrawal of " + withdrawalAmount + " processed. Remaining balance is " + this.balance);
+        }
+    }
+
+    //generate getters and setters quickly
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmailAddress() {
+        return customerEmailAddress;
+    }
+
+    public void setCustomerEmailAddress(String customerEmailAddress) {
+        this.customerEmailAddress = customerEmailAddress;
+    }
+
+    public String getCustomerPhoneNumber() {
+        return customerPhoneNumber;
+    }
+
+    public void setCustomerPhoneNumber(String customerPhoneNumber) {
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
+}
---
+++ Constructors/src/academy/learnprogrammng/Main.java
@@ -0,0 +1,27 @@
+package academy.learnprogrammng;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Account bobsAccount = new Account(); //("12345", 0.00, "Bob Brown", "myemail@bob.com", "087-123-4567");
+
+        //redundant, don't need to do a constructor
+//        bobsAccount.setNumber("12345");
+//        bobsAccount.setBalance(0.00);
+//        bobsAccount.setCustomerName("Bob Brown");
+//        bobsAccount.setCustomerEmailAddress("myemail@bob.com");
+//        bobsAccount.setCustomerPhoneNumber("087.1233.555");
+
+        System.out.println(bobsAccount.getNumber());
+        System.out.println(bobsAccount.getBalance());
+
+        bobsAccount.withdrawal(100.0);
+
+        bobsAccount.deposit(50.0);
+        bobsAccount.withdrawal(100.0);
+
+        bobsAccount.deposit(51.0);
+        bobsAccount.withdrawal(100.0);
+    }
+}
