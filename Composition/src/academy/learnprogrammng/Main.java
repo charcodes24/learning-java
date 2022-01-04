394a69aab222490da05e97094ad86021828079b8
charcodes24 <charcodes24@gmail.com>
2022-01-03 19:04:38 -0800

    Composition challenge

--- Composition/src/academy/learnprogrammng/Main.java
+++ Composition/src/academy/learnprogrammng/Main.java
@@ -13,10 +13,31 @@ public class Main {
         Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
 
         //COMPOSITION EXAMPLE
+        //composition: creating objects within objects; used instead of inheritance in some cases
+        //COMPOSITION should be looked at first vs inheritance
+        //gives you added advantages and more flexibility
         PC thePC = new PC(theCase, theMonitor, theMotherboard); // created PC class by passing other objects as parameters
-        thePC.getMonitor().drawPixelAt(1500, 1200, "red");
-        thePC.getMotherboard().loadProgram("Windows 1.0");
-        thePC.getTheCase().pressPowerButton();
+//        thePC.getMonitor().drawPixelAt(1500, 1200, "red");
+//        thePC.getMotherboard().loadProgram("Windows 1.0");
+//        thePC.getTheCase().pressPowerButton();
+        thePC.powerUp();
+
+
+        Wall wall1 = new Wall("West");
+        Wall wall2 = new Wall("East");
+        Wall wall3 = new Wall("South");
+        Wall wall4 = new Wall("North");
+
+        Ceiling ceiling = new Ceiling(12, 55);
+
+        Bed bed = new Bed("Modern", 4, 3, 2, 1);
+
+        Lamp lamp = new Lamp("Classic", false, 75);
+
+        Bedroom bedroom = new Bedroom("Charlene's", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
+
+        bedroom.makeBed();
+        bedroom.getLamp().turnOn();
 
     }
 }
