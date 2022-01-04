8033c76a0c6e31df918f9b00fd76922d0726a9b2
charcodes24 <charcodes24@gmail.com>
2022-01-03 18:49:23 -0800

    Java Composition explained with examples

---
+++ Composition/src/academy/learnprogrammng/Main.java
@@ -0,0 +1,22 @@
+package academy.learnprogrammng;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Dimensions dimensions = new Dimensions(20, 20, 5);
+        Case theCase = new Case("220B", "Dell", "240", dimensions);
+
+
+        Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540, 1440));
+
+        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
+
+        //COMPOSITION EXAMPLE
+        PC thePC = new PC(theCase, theMonitor, theMotherboard); // created PC class by passing other objects as parameters
+        thePC.getMonitor().drawPixelAt(1500, 1200, "red");
+        thePC.getMotherboard().loadProgram("Windows 1.0");
+        thePC.getTheCase().pressPowerButton();
+
+    }
+}
