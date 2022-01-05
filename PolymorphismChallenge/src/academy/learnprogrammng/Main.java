6b3ecaa62369dae7ff717d428632b5084b63bf38
charcodes24 <charcodes24@gmail.com>
2022-01-05 14:57:44 -0800

    Completed polymorphism challenge

---
+++ PolymorphismChallenge/src/academy/learnprogrammng/Main.java
@@ -0,0 +1,81 @@
+package academy.learnprogrammng;
+
+class Car {
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public Car(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = name;
+        this.wheels = 4;
+        this.engine = true;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String startEngine() {
+        return "Car -> startEngine()";
+    }
+
+    public String accelerate() {
+        return "Car -> accelerate()";
+    }
+
+    public String brake() {
+        return "Car -> brake()";
+    }
+}
+
+class Mitsubhisi extends Car {
+
+    public Mitsubhisi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Mitsubishi -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Mitsubishi -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Mitsubishi -> brake()";
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Car car = new Car(8, "Base car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+
+        Mitsubhisi mitsubhisi = new Mitsubhisi(6, "Outlander VRX 4WD");
+        System.out.println(mitsubhisi.startEngine());
+        System.out.println(mitsubhisi.accelerate());
+        System.out.println(mitsubhisi.brake());
+
+        Ford ford = new Ford(6, "Ford Falcon");
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+
+
+    }
+}
