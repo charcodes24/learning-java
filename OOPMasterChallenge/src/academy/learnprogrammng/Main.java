60305cd9831212475de937d11131a07b196f8f88
charcodes24 <charcodes24@gmail.com>
2022-01-05 15:37:16 -0800

    OOP Master CHallenge phase 1 completed

---
+++ OOPMasterChallenge/src/academy/learnprogrammng/Main.java
@@ -0,0 +1,16 @@
+package academy.learnprogrammng;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        double price = hamburger.itemizeHamburger();
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.12);
+        price = hamburger.itemizeHamburger();
+        System.out.println(price);
+    }
+}
